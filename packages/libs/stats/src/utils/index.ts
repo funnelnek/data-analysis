9375dd7b34bcfd11360cc63287e579ef5389e76c
Robert Banks <seoinsighters@gmail.com>
2023-07-16 10:15:41 +0000

    refactored themes package

---
+++ packages/libs/stats/src/utils/index.ts
@@ -0,0 +1,15 @@
+export function isContinuous() {
+    throw new Error("Not Implemented");
+}
+
+export function isDiscrete() {
+    throw new Error("Not Implemented");
+}
+
+export function isDimension() {
+    throw new Error("Not Implemented");
+}
+
+export function isMetric() {
+    throw new Error("Not Implemented");
+}
