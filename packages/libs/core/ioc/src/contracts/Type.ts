408045e35f7f2c444e6d8d95d83081d890e33654
Robert Banks <seoinsighters@gmail.com>
2023-07-16 06:21:06 +0000

    Refactored  core/ioc Type interface

--- packages/libs/core/ioc/src/contracts/Type.ts
+++ packages/libs/core/ioc/src/contracts/Type.ts
@@ -1,4 +1,15 @@
-export interface Type<T = any> extends Function {
+/**
+ * Generic constructor class
+ */
+export interface Type<T extends {} = {}> extends Function {
+    /**
+     * The constructor method.
+     * @param args - The constructor parameters.
+     * @returns {T} - The class instance type.
+     */
     new (...args: any[]): T;
+    /**
+     * The class prototype.
+     */
     prototype: T;
 }
