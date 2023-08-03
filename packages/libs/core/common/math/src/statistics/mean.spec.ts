b19f9e3eeaf43d3b8777338050a72387ad77be73
Robert Banks <seoinsighters@gmail.com>
2023-08-03 01:51:24 +0000

    Modified mean function testing

--- packages/libs/core/common/math/src/statistics/mean.spec.ts
+++ packages/libs/core/common/math/src/statistics/mean.spec.ts
@@ -1,26 +1,24 @@
-import { jest } from '@jest/globals';
 import { mean } from './mean';
-import { InvalidArgumentError, MissingArgumentError } from '@funnelnek/exceptions';
+import { InvalidArgumentError } from '@funnelnek/exceptions';
 
-let mockSum = jest.fn(() => 45);
+// Calculating the expected results.
+let data = [35, 40, 45, 49, 34, 47, 39, 25, 19, 35, 28, 48];
+let avg = data.reduce((a, b) => a + b) / data.length;
+let invalid: unknown[] = [
+    [],
+    '0',
+    undefined
+];
 
-jest.mock('../arithmetic/sum', () => ({
-    __esModule: true,
-    sum: mockSum
-}));
 
 describe('mean', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
-    it("should throw InvalidArgumentError if series is an invalid data type", () => {        
-        expect(() => mean([])).toThrow(InvalidArgumentError);
-        expect(() => mean('0' as unknown as number[])).toThrow(InvalidArgumentError);
-        expect(() => mean(undefined!)).toThrow(InvalidArgumentError);
+    it("should throw InvalidArgumentError if series is an invalid data type", () => {    
+        invalid.forEach((x) => {
+            expect(() => mean(x as number[])).toThrow(InvalidArgumentError);
+        });
     });
 
     it("should calculate the mean from an array of numbers", () => {
-        expect(mean([1,2,3,4,5,6,7,8,9])).toBe(5);
+        expect(mean(data)).toBe(avg);
     });
 });
--- packages/libs/core/common/math/src/statistics/mean.ts
+++ packages/libs/core/common/math/src/statistics/mean.ts
@@ -3,8 +3,14 @@ import { sum } from "../arithmetic";
 /**
  * Calculate the mean/average from a series of numbers.
  * 
+ * @remarks
+ * In statistics, Arithmetic Mean is the average of all data values which you work with. 
+ * Mean is used to find the average value around which your data values range.
+ * 
+ * To find the mean, all you have to do is add up all the values in your data and then divide it by the total number of data values. 
+ * 
+ * 
  * @example
- *
  * ```ts
  * mean([1,3,5,7,9]) // 5
  * ```
